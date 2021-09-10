aef786fb613e39e9f00a60d1f0418e33275ed6c1
chethanksd <chetandev.ksd@gmail.com>
2021-09-10 12:26:38 +0530

    removed unwanted files

--- src/extralib.mk
+++
@@ -1,8 +0,0 @@
-#
-# makefile to help integrate 3rd party library
-#
-
-OPENCV_LIB += -lopencv_core 
-OPENCV_LIB += -lopencv_highgui
-OPENCV_LIB += -lopencv_imgproc 
-OPENCV_LIB += -lopencv_imgcodecs
